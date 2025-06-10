8ab8ff4ea3fe3980daa20982aadf43f8fda4323b
vermilion10 <180294769+vermilion10@users.noreply.github.com>
2025-06-10 15:25:08 +0700

    adding keterangan module at salt header

--- salt.h
+++ salt.h
@@ -19,12 +19,12 @@ typedef struct Salt {
 
 // Function prototypes interface
 Salt* create_salt(const char* name, const char* cation, int cat_charge, 
-                 const char* anion, int an_charge);
-void add_salt(Salt** salt_list, Salt* new_salt);
-Salt* find_salt(Salt* salt_list, const char* name);
-void print_salts(Salt* salt_list);
-void read_salts_from_file(Salt** salt_list, const char* filename);
-void free_salt_list(Salt* salt_list);
+                 const char* anion, int an_charge); //buat garam baru
+void add_salt(Salt** salt_list, Salt* new_salt);//tambah garam ke list
+Salt* find_salt(Salt* salt_list, const char* name);//cari garam berdasarkan nama
+void print_salts(Salt* salt_list); //print semua garam
+void read_salts_from_file(Salt** salt_list, const char* filename); //read garam dari file
+void free_salt_list(Salt* salt_list);//dealloc memori list garam
 
 
 #endif
