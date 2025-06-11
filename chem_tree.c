e20eca86a3cf5a1d7c356a179b49a4600b3dc560
Dyooooou <juliandiokhusussekolah@gmail.com>
2025-06-11 19:33:17 +0700

    declare tree node

--- chem_tree.c
+++ chem_tree.c
@@ -2,3 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+// Deklarasi 2 tree secara global to make it always empty 
+TreeNode* periodic_tree_by_atomic = NULL;
+TreeNode* periodic_tree_by_name = NULL;
+
