44660dff96d3b7addc85df9c9d21a7bebd7c6107
Dyooooou <juliandiokhusussekolah@gmail.com>
2025-06-11 20:55:58 +0700

    add the comparator function

--- chem_tree.c
+++ chem_tree.c
@@ -6,3 +6,11 @@
 TreeNode* periodic_tree_by_atomic = NULL;
 TreeNode* periodic_tree_by_name = NULL;
 
+// Fungsi perbandingan
+int compare_by_atomic_number(const Element* a, const Element* b) {
+    return a->atomic_number - b->atomic_number;
+}
+
+int compare_by_name(const Element* a, const Element* b) {
+    return strcmp(a->name, b->name);
+}
