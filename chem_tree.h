cab01cc39c20e4be7f28e9da3988674605e01cf9
Dyooooou <juliandiokhusussekolah@gmail.com>
2025-06-17 01:05:54 +0700

    back to old module

--- chem_tree.h
+++ chem_tree.h
@@ -11,20 +11,11 @@ typedef struct TreeNode {
     struct TreeNode* right;
 } TreeNode;
 
-// Function pointer untuk comparator
-typedef int (*CompareFunc)(const Element*, const Element*);
-
-// Function prototype interface
 TreeNode* create_tree_node(Element elem);
-TreeNode* insert_to_tree(TreeNode* root, Element elem, CompareFunc compare);
-Element* search_element(TreeNode* root, void* key, CompareFunc compare);
-// Buld 2 different tree based on the atomic number/name
-Element* search_by_atmnumb(int atomic_number);
-Element* search_by_name(char* name);
-
-
-void build_periodic_tree(TreeNode** tree_by_atomic, TreeNode** tree_by_name, const Element* table, int count);
-void process_inorder(TreeNode* root);
+TreeNode* insert_to_tree(TreeNode* root, Element elem);
+Element* search_element(TreeNode* root, int atomic_number);
+void build_periodic_tree(TreeNode** root, Element* table, int count);
+void inorder_traversal(TreeNode* root);
 void print_tree_table(TreeNode* root);
 void free_tree(TreeNode* root);
 
