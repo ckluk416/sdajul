f8b5abd4c88a717f827e3fc16e3713ef069e644d
Dyooooou <juliandiokhusussekolah@gmail.com>
2025-06-17 01:07:47 +0700

    add the implementation

--- chem_tree.c
+++ chem_tree.c
@@ -2,47 +2,34 @@
 #include <string.h>
 #include <stdlib.h>
 
-// Deklarasi 2 tree secara global to make it always empty 
-TreeNode* periodic_tree_by_atomic = NULL;
-TreeNode* periodic_tree_by_name = NULL;
-
-// Fungsi perbandingan
-int compare_by_atomic_number(const Element* a, const Element* b) {
-    return a->atomic_number - b->atomic_number;
-}
-
-int compare_by_name(const Element* a, const Element* b) {
-    return strcmp(a->name, b->name);
-}
-
-TreeNode* create_tree_node(Element elem){
+TreeNode* create_tree_node(Element elem) {
     TreeNode* new_node = (TreeNode*)malloc(sizeof(TreeNode));
-    if (new_node){
-        new_node->data = elem;
-        new_node->left = new_node->right = NULL;
-    
+    if (!new_node) {
+        printf("Error: malloc failed!\n");
+        exit(EXIT_FAILURE);
     }
+    new_node->data = elem;
+    new_node->left = new_node->right = NULL;
     return new_node;
 }
 
-TreeNode* insert_to_tree(TreeNode* root, Element elem, CompareFunc compare) {
-    if (root == NULL) {
-        return create_tree_node(elem);
-    
-    int cmp = compare(&elem, &root->data);
-    if (cmp < 0){
-        root->left = insert_to_tree(root->left, elem, compare);
-    } else if (cmp > 0){
-        root->right = insert_to_tree(root->right, elem, compare);
-    }
-    
+// Insert element to BST by atomic number
+TreeNode* insert_to_tree(TreeNode* root, Element elem) {
+    if (root == NULL) return create_tree_node(elem);
+    if (elem.atomic_number < root->data.atomic_number)
+        root->left = insert_to_tree(root->left, elem);
+    else if (elem.atomic_number > root->data.atomic_number)
+        root->right = insert_to_tree(root->right, elem);
     return root;
 }
 
-Element* search_element(TreeNode* root, void* key, CompareFunc compare) {
-    if (root == NULL) {
-        return NULL;
-    }
-
-    
+// Search element by atomic number
+Element* search_element(TreeNode* root, int atomic_number) {
+    if (root == NULL) return NULL;
+    if (atomic_number == root->data.atomic_number)
+        return &(root->data);
+    else if (atomic_number < root->data.atomic_number)
+        return search_element(root->left, atomic_number);
+    else
+        return search_element(root->right, atomic_number);
 }
