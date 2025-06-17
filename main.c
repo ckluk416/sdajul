094368b6b4eb3082155fd387e74ff77b3024141e
Aang-657 <gilangmaulana657@gmail.com>
2025-06-17 19:25:53 +0700

    init connect all file

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "element.h"
+#include "bst.h"
+#include "salt.h"
+#include "stack.h"
+#include "queue.h"
+#include "electron_config.h"
+
+#define ELEMENT_FILE "tabel_periodik.txt"
+#define SALT_FILE "Daftar_Garam.txt"
+
+int main(){
+    
+    return 0;
+}
